41104182829eb911ac677eabc2ea97d20260380a
Hanintsoa Cathya Edith Andrianjaka <haniandr@c1r10s11.42antananarivo.mg>
2026-06-16 14:21:08 +0300

    add these files

--- ex1/loading.py
+++ ex1/loading.py
@@ -3,16 +3,6 @@ from importlib.util import find_spec
 from importlib.metadata import version
 
 
-try:
-    import numpy as np
-    import pandas as pd
-    from pandas import DataFrame
-    import matplotlib as mpt
-except ImportError as e:
-    print(f"Error: {e}")
-    exit(1)
-
-
 def check_dependancies() -> None:
     dependance: dict[str, str] = {
             "numpy": "Data manipulation ready",
@@ -29,20 +19,29 @@ def check_dependancies() -> None:
             print(f"[MISSING] {pkg}")
 
 
-def generate_matrix_data(seed: int | None) -> DataFrame:
-    if seed is not None:
-        np.random.seed(seed)
-    age = np.random.randint(0, 101, size=1000)
-    temp = np.random.randint(0, 101, size=1000)
-    color = np.random.randint(0, 101, size=1000)
-
-    return pd.DataFrame({
-            "age": age,
-            "temperature": temp, 
-            "color": color
-        })
-
 def loading() -> None:
+    try:
+        import numpy as np
+        import pandas as pd
+        from pandas import DataFrame
+        import matplotlib.pyplot as plt
+    except ImportError as e:
+        print(f"Error: {e}")
+        exit(1)
+
+    def generate_matrix_data(seed: int | None) -> DataFrame:
+        if seed is not None:
+            np.random.seed(seed)
+        age = np.random.randint(0, 101, size=1000)
+        temp = np.random.randint(0, 101, size=1000)
+        color = np.random.randint(0, 101, size=1000)
+
+        return pd.DataFrame({
+                "age": age,
+                "temperature": temp, 
+                "color": color
+            })
+
     filename = "matrix_analyzis.png"
 
     print("\nLOADING STATUS: Loading program...\n")
@@ -60,7 +59,7 @@ def loading() -> None:
     plt.figure(figsize=(10, 10))
     plt.scatter(
             df["age"],
-            df["temp"],
+            df["temperature"],
             c=df["color"],
             alpha=0.6
         )
@@ -68,6 +67,7 @@ def loading() -> None:
     plt.xlabel("age")
     plt.ylabel("temp")
     plt.colorbar(label="color")
+    plt.show()
     plt.savefig(filename)
     plt.close()
 
