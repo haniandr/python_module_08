3c1abb281aa48ccc139966dff1b45ec3c8a2d9b9
Cathya Andrianjaka <cathyaandrianjaka@gmail.com>
2026-06-21 18:15:19 +0300

    Update oracle.py

--- ex2/oracle.py
+++ ex2/oracle.py
@@ -38,7 +38,7 @@ def show_configuration() -> None:
             var = ("Connected to local instance"
                    if var == "localhost" or (not var)
                    else var)
-        if not (var == "development" or "production"):
+        if var != "development" or var != "production":
             print("WARNING - Unknown MATRIX_MODE")
         print(f"{matrix_dic[item]}: {var}")
 
